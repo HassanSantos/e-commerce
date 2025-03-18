775ba960499deb8f1d3ecd85984d0d49fe02d52a
hassan <hassanssena@gmail.com>
2025-03-18 13:41:07 -0300

    feat: Alterando para a estrutura mvc

--- src/main/java/com/foursales/e_commerce/controller/ProductController.java
+++ src/main/java/com/foursales/e_commerce/controller/ProductController.java
@@ -1,6 +1,6 @@
 package com.foursales.e_commerce.controller;
 
-import com.foursales.e_commerce.application.usecase.product.*;
+import com.foursales.e_commerce.domain.service.ProductService;
 import com.foursales.e_commerce.domain.service.model.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,37 +9,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public record ProductController(CreateProductUseCase createProductUseCase,
-                                ListProductOrdersUseCase listProductOrdersUseCase,
-                                DeleteProductUseCase deleteProductUseCase,
-                                FindProductUseCase findProductUseCase,
-                                UpdateProductUseCase updateProductUseCase) {
+public record ProductController(ProductService productService) {
 
     @PostMapping("/products")
     public ResponseEntity<String> createProduct(@RequestBody Product product) {
-        createProductUseCase.createProduct(product);
+        productService.createProduct(product);
         return ResponseEntity.status(201).build();
     }
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> listProducts() {
-        return ResponseEntity.ok(listProductOrdersUseCase.getAllProducts());
+        return ResponseEntity.ok(productService.getAllProducts());
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Product> findProduct(@PathVariable String id) {
-        return ResponseEntity.ok(findProductUseCase.exect(id));
+        return ResponseEntity.ok(productService.getProductById(id));
     }
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable String id) {
-        deleteProductUseCase.deleteProduct(id);
+        productService.deleteProduct(id);
         return ResponseEntity.ok("ok");
     }
 
     @PutMapping("/products/{id}")
     public ResponseEntity<String> updateProduct(@PathVariable String id, @RequestBody Product product) {
-        updateProductUseCase.execute(product);
+        productService.updateProduct(product);
         return ResponseEntity.ok("ok");
     }
 }
